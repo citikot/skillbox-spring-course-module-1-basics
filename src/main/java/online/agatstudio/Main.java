659230ddfab71d8e10612637eb99b9c03b8585f0
citikot <citikot@dr.com>
2025-02-16 13:03:21 +0200

    added comments

--- src/main/java/online/agatstudio/Main.java
+++ src/main/java/online/agatstudio/Main.java
@@ -3,11 +3,11 @@ package online.agatstudio;
 public class Main {
     public static void main(String[] args) {
 
-        BeanFactory beanFactory = new BeanFactory();
-        beanFactory.register(Injectable.class);
-        beanFactory.register(Source.class);
+        BeanFactory beanFactory = new BeanFactory(); // создали контейнер
+        beanFactory.register(Injectable.class); // регистрируем один бин
+        beanFactory.register(Source.class); // регистрируем второй бин
 
-        Source source = beanFactory.getBean(Source.class);
-        source.call();
+        Source source = beanFactory.getBean(Source.class); // получаем бин Source из контейнера
+        source.call(); // вызываем метод на бине, который вызывает метод на другом бине
     }
 }
