0579d3416db6909296f02fc6bc60ece3f8d0ec3e
citikot <citikot@dr.com>
2025-03-06 12:40:11 +0200

    fixed package name

--- src/main/java/online/agatstudio/spring_basics/app_profiles/config/DefaultAppConfig.java
+++ src/main/java/online/agatstudio/spring_basics/app_profiles/config/DefaultAppConfig.java
@@ -1,8 +1,5 @@
 package online.agatstudio.spring_basics.app_profiles.config;
 
-import online.agatstudio.spring_basics.app_profiles.DefaultEnvPrinter;
-import online.agatstudio.spring_basics.app_profiles.EnvPrinter;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
